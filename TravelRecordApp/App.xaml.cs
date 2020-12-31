da810177dbe7f56ae7b4d68af00b681afbf1a094
Bryant Omoregie <unclebryant@gmail.com>
2020-12-31 09:04:21 -0600

    Not saving to database.

--- TravelRecordApp/App.xaml.cs
+++ TravelRecordApp/App.xaml.cs
@@ -6,11 +6,23 @@ namespace TravelRecordApp
 {
     public partial class App : Application
     {
+        public static string DatabaseLocation = string.Empty;
         public App()
         {
             InitializeComponent();
 
             MainPage = new NavigationPage(new MainPage());
+        }
+
+        public App(string databaseLocation)
+        {
+            InitializeComponent();
+
+            DatabaseLocation = databaseLocation;
+
+            MainPage = new NavigationPage(new MainPage());
+
+            
         }
 
         protected override void OnStart()
